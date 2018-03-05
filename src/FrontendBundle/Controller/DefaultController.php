d150410df89277691d431b25a131b79c56b0e453
Djelka <delabreflorian1995@gmail.com>
2018-03-05 22:32:22 +0100

    Add Connect Features (No Reg)

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -8,6 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('FrontendBundle:Default:index.html.twig');
+        $formFactory = $this->get('fos_user.registration.form.factory');
+        $form = $formFactory->createForm();
+        if ($this->has('security.csrf.token_manager')) {
+            $csrfToken = $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue();
+        } else {
+            // BC for SF < 2.4
+            $csrfToken = $this->has('form.csrf_provider')
+                ? $this->get('form.csrf_provider')->generateCsrfToken('authenticate')
+                : null;
+        }
+        return $this->render('FrontendBundle:Default:index.html.twig', array(
+            'form' => $form->createView(),
+            'csrf_token' => $csrfToken,
+        ));
     }
 }
